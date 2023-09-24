7fbf04c99959ccacbe9d18c2291af4f5da5d49d0
idnc_sk <me@idnc.sk>
2023-09-24 12:39:10 +0200

    Reorg v1

---
+++ app/managers/context/layers/Canvas.js
@@ -0,0 +1,8 @@
+
+const Layer = require('../lib/Layer')
+
+class Canvas extends Layer {
+
+}
+
+module.exports = Canvas
---
+++ app/managers/context/layers/Filter.js
@@ -0,0 +1,8 @@
+
+const Layer = require('../lib/Layer')
+
+class Filter extends Layer {
+
+}
+
+module.exports = Filter
