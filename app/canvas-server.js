b9fd1239c5464295205df426f7c5b6f029f50b94
idnc_sk <me@idnc.sk>
2023-10-29 16:31:59 +0100

    sync

--- app/canvas-server.js
+++ app/canvas-server.js
@@ -5,62 +5,15 @@
 // Utils
 const path = require('path')
 const os = require('os')
-const minimist = require('minimist')
+
+// CLI
+const cli = require('./ui/cli/server')
 
 // Canvas
 const Canvas = require('./main')
+const canvas = new Canvas();
 
-// Parse command line arguments
-const args = minimist(process.argv.slice(2), {
-  alias: {
-    'help': 'h',
-    'version': 'v',
-    'paths': 'p'
-  }
-})
-
-if (args.help) {
-  console.log('Usage: canvas-server [options]');
-  console.log('h, help         Print this help');
-  console.log('v, version      Print version');
-  console.log('p, paths        Print paths');
-  process.exit(0)
-}
-
-// Start canvas
+// Start
 (async () => {
-  const canvas = new Canvas();
   await canvas.start();
-
-  const context = canvas.createContext("/foo/bar/baz");
-  console.log(context.listDocuments());
-  context.set('/')
-  console.log(await context.insertDocument({
-    type: "data/abstraction/tab",
-    data: {
-      url: "https://www.test.com/search?q=canvas+server",
-      title: "canvas test /foo/bar/baz",
-    },
-  }))
-  console.log(context.listDocuments());
-
-  context.set('/foo/baz/bar')
-  console.log(await context.insertDocument({
-    type: "data/abstraction/tab",
-    data: {
-      url: "https://www.test.com/search?q=canvas+server",
-      title: "canvas test /foo/baz/bar",
-    },
-  }))
-
-  console.log(context.listDocuments());
-
-  context.set('/foo')
-  console.log(context.listDocuments());
-
-  //canvas.shutdown();
-
 })();
-
-
-
