78b299f5016dc49323b381a068d3893e4ab3107a
idnc_sk <me@idnc.sk>
2024-06-15 23:57:42 +0200

    reorg

---
+++ src/main/RoleManager.js
@@ -0,0 +1,23 @@
+
+// https://www.npmjs.com/package/dockerode
+// https://github.com/apocas/dockerode-compose
+
+class RoleManager {
+
+    constructor() {}
+
+    listRoles() {}
+
+    startRole() { }
+
+    stopRole() { }
+
+    restartRole() { }
+
+    removeRole() { }
+
+    addRole() { }
+
+    attachRole() { }
+
+}
