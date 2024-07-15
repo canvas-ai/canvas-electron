79a60fbf00c3c216134a8aa5b0065cacfb95e2d1
idnc_sk <me@idnc.sk>
2024-07-15 18:08:57 +0200

    sync

---
+++ src/components/canvas/index.js
@@ -0,0 +1,7 @@
+
+
+
+class Canvas { }
+
+
+module.exports = Canvas;
---
+++ src/components/toolbox/index.js
@@ -0,0 +1,28 @@
+
+
+
+class Toolbox extends EventEmitter {
+    constructor() {
+        super();
+        this.tools = [];
+    }
+
+    register(tool) {
+        if (!tool.name) {throw new Error('Tool name is mandatory');}
+        if (!tool.icon) {throw new Error('Tool icon is mandatory');}
+        if (!tool.action) {throw new Error('Tool action is mandatory');}
+        this.tools.push(tool);
+        this.emit('tool-registered', tool);
+    }
+
+    unregister(tool) {
+        const index = this.tools.indexOf(tool);
+        if (index === -1) {return;}
+        this.tools.splice(index, 1);
+        this.emit('tool-unregistered', tool);
+    }
+
+    toggle() {
+        this.emit('toolbox-toggled');
+    }
+}
